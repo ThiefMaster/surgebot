1149c03b7a26c1187845105a628f64e96f6e0f96
ThiefMaster <thiefmaster@gamesurge.net>
2006-05-13 11:04:23 +0000

    - Added connect timeout - Added reconnect timer

--- irc.c
+++ irc.c
@@ -1,14 +1,19 @@
 #include "global.h"
 #include "sock.h"
 #include "irc.h"
+#include "timer.h"
 
 extern struct surgebot_conf bot_conf;
+extern int quit_poll;
 
 static void irc_sock_read(struct sock *sock, char *buf, size_t len);
 static void irc_sock_event(struct sock *sock, enum sock_event event, int err);
 static void irc_connected();
 static void irc_disconnected();
+static void irc_connect_timeout(void *bound, void *data);
 static void irc_error(int err);
+static void irc_schedule_reconnect(unsigned int wait);
+static void irc_reconnect(void *bound, void *data);
 static void irc_handle_msg(int argc, char **argv, struct irc_source *src);
 static char *irc_format_line(const char *msg);
 
@@ -36,39 +41,91 @@ int irc_connect()
 	if(bot.server_sock == NULL)
 		return -2;
 
+	debug("Connecting to %s:%d", bot_conf.server_host, bot_conf.server_port);
 	res = sock_connect(bot.server_sock, bot_conf.server_host, bot_conf.server_port);
 	if(res != 0)
 		return -3;
 
 	sock_set_readbuf(bot.server_sock, MAXLEN, "\r\n");
 
-	// TODO: Add connect timeout
+	timer_add(&bot, "server_connect_timeout", now + 15, irc_connect_timeout, bot.server_sock, 0);
 	return 0;
 }
 
 static void irc_connected()
 {
+	timer_del_boundname(&bot, "server_connect_timeout");
+
 	if(bot_conf.server_pass)
 		irc_send("PASS :%s", bot_conf.server_pass);
 
 	irc_send("USER %s * * :%s", bot_conf.username, bot_conf.realname);
 	irc_send("NICK %s", bot_conf.nickname);
 
+	if(bot.nickname) free(bot.nickname);
+	if(bot.username) free(bot.username);
+	if(bot.realname) free(bot.realname);
+	if(bot.hostname) free(bot.hostname);
+
 	bot.nickname = strdup(bot_conf.nickname);
 	bot.username = strdup(bot_conf.username);
 	bot.realname = strdup(bot_conf.realname);
+	bot.hostname = NULL;
 }
 
 static void irc_disconnected()
 {
+	timer_del_boundname(&bot, "server_connect_timeout");
 	bot.server_sock = NULL;
-	// TODO: Schedule reconnect
+	irc_schedule_reconnect(5);
+}
+
+static void irc_connect_timeout(void *bound, void *data)
+{
+	if(data != bot.server_sock)
+	{
+		log_append(LOG_WARNING, "Ignoring old connect timeout (%p != %p)", data, bot.server_sock);
+		return;
+	}
+
+	debug("Could not connect to server - timeout");
+	sock_close(bot.server_sock);
+	bot.server_sock = NULL;
+	irc_schedule_reconnect(30);
 }
 
 static void irc_error(int err)
 {
+	timer_del_boundname(&bot, "server_connect_timeout");
 	bot.server_sock = NULL;
-	// TODO: Schedule reconnect
+	irc_schedule_reconnect(15);
+}
+
+static void irc_schedule_reconnect(unsigned int wait)
+{
+	if(timer_exists_boundname(&bot, "server_reconnect"))
+		return;
+
+	bot.server_tries++;
+	if(bot_conf.max_server_tries && bot.server_tries > bot_conf.max_server_tries)
+	{
+		log_append(LOG_WARNING, "Max. server connect attempts (%d) exceeded - exiting", bot_conf.max_server_tries);
+		quit_poll = 1;
+		return;
+	}
+
+	timer_add(&bot, "server_reconnect", now + wait, irc_reconnect, NULL, 0);
+}
+
+static void irc_reconnect(void *bound, void *data)
+{
+	if(bot_conf.max_server_tries > 0)
+		debug("Reconnecting (%d reconnect attempt(s) left)", bot_conf.max_server_tries - bot.server_tries);
+	else
+		debug("Reconnecting");
+
+	// TODO: delete all users/channels
+	irc_connect();
 }
 
 // TODO: It might be a good idea to move that into a separate source file
@@ -86,6 +143,7 @@ static void irc_handle_msg(int argc, char **argv, struct irc_source *src)
 	{
 		assert(argc > 1);
 		irc_send("PONG :%s", argv[1]);
+		bot.server_tries = 0; // TODO: that should be done when getting the 001 numeric
 	}
 }
 
