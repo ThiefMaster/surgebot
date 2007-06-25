d9be29e3e52ee9ee853067f87526e656a865805a
Someone <someone@surgebot.net>
2007-06-25 09:32:08 +0000

    - Minor fixes and improvements - Updated helpfile for command "loginmask"

--- modules/commands/commands.c
+++ modules/commands/commands.c
@@ -361,7 +361,6 @@ static void handle_command(struct irc_source *src, struct irc_user *user, struct
 		free(new_msg);
 
 		log_append(LOG_CMD, "%s", log_entry->string);
-		irc_send("NOTICE @#surgebot.intern :%s", log_entry->string);
 		stringbuffer_free(log_entry);
 	}
 	else
@@ -495,12 +494,10 @@ static int binding_expand_alias(struct cmd_binding *binding, struct irc_source *
 static int binding_check_access(struct irc_source *src, struct irc_user *user, struct irc_channel *channel, char *channelname, struct cmd_binding *binding, unsigned int quiet)
 {
 	enum command_rule_result res;
-	unsigned char authed = 1;
 	char *user_mask;
 
 	if(user && !user->account)
 	{
-		authed = 0;
 		struct dict *accounts = account_dict();
 		user_mask = malloc(strlen(src->ident) + strlen(src->host) + 2);
 		sprintf(user_mask, "%s@%s", src->ident, src->host);
@@ -512,7 +509,6 @@ static int binding_check_access(struct irc_source *src, struct irc_user *user, s
 				account_user_add(acc, user);
 				irc_send("PRIVMSG #surgebot.intern :User $b%s$b (%s) has automatically been authed to account $b%s$b, matching loginmask (%s)", src->nick, user_mask, acc->name, acc->login_mask);
 				debug("Auto-Auth by user %s (%s) to account %s (loginmask %s)", src->nick, user_mask, acc->name, acc->login_mask);
-				authed = 1;
 				break;
 			}
 		}
@@ -526,7 +522,7 @@ static int binding_check_access(struct irc_source *src, struct irc_user *user, s
 		return 0;
 	}
 
-	if(!authed && (binding->cmd->flags & CMD_REQUIRE_AUTHED))
+	if((!user || !user->account) && (binding->cmd->flags & CMD_REQUIRE_AUTHED))
 	{
 		if(!quiet && (!command_conf.stealth || (user && user->account)))
 			reply("You must be authed to use $b%s$b.", binding->name);
