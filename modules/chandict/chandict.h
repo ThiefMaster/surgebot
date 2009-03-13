9f8cfbeb6c40fcf73a99a9a761efe5cafe991c50
ThiefMaster <thiefmaster@gamesurge.net>
2009-03-13 22:55:06 +0000

    Add missing include-once defines to chandict.h.

--- modules/chandict/chandict.h
+++ modules/chandict/chandict.h
@@ -1,3 +1,8 @@
+#ifndef CHANDICT_H
+#define CHANDICT_H
+
 struct dict *chandict_get_entries(const char *channel);
 void chandict_add_entry(const char *channel, const char *entry, const char *data);
 void chandict_del_entry(const char *channel, const char *entry);
+
+#endif
