54142e968a2fac71a88bf586f57306c4af51a4ff
ThiefMaster <thiefmaster@gamesurge.net>
2009-11-21 15:36:11 +0000

    Add missing semicolon

--- modules/chanserv_users/chanserv_users.c
+++ modules/chanserv_users/chanserv_users.c
@@ -581,7 +581,7 @@ static void chanserv_parse_name(struct chanserv_channel *cschan, const char *ite
 
 	access = strtoul(item, &ptr2, 10);
 	if(*ptr2 != ':')
-		log_append(LOG_WARNING, "Odd item in chanserv_parse_name: %s - *ptr2 = %c (%u)", item, *ptr2, *ptr2)
+		log_append(LOG_WARNING, "Odd item in chanserv_parse_name: %s - *ptr2 = %c (%u)", item, *ptr2, *ptr2);
 
 	assert(*ptr2 == ':');
 	assert(access <= 500);
