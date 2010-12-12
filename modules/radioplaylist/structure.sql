002363b24781e2d80a763dd3f1ff3f3630cd6bbc
ThiefMaster <thiefmaster@gamesurge.net>
2010-12-12 15:27:15 +0100

    Update radioplaylist database structure

--- modules/radioplaylist/structure.sql
+++ modules/radioplaylist/structure.sql
@@ -19,3 +19,33 @@ WITH (
 CREATE INDEX playlist_blacklist_index ON playlist USING btree (blacklist);
 CREATE INDEX playlist_duration_idx ON playlist USING btree (duration);
 CREATE UNIQUE INDEX playlist_file_idx ON playlist USING btree (file);
+
+
+
+CREATE TABLE genres
+(
+	id serial NOT NULL,
+	genre character varying(32) NOT NULL,
+	min_votes smallint NOT NULL DEFAULT 1,
+	CONSTRAINT genres_pkey PRIMARY KEY (id)
+)
+WITH (
+	OIDS=FALSE
+);
+
+CREATE UNIQUE INDEX genre_unique_ci ON genres USING btree (lower(genre::text));
+
+
+
+CREATE TABLE song_genres
+(
+	genre_id integer NOT NULL,
+	song_id integer NOT NULL,
+	CONSTRAINT song_genres_pkey PRIMARY KEY (genre_id, song_id),
+	CONSTRAINT song_genres_genre_id_fkey FOREIGN KEY (genre_id) REFERENCES genres (id) MATCH SIMPLE ON UPDATE NO ACTION ON DELETE CASCADE,
+	CONSTRAINT song_genres_song_id_fkey FOREIGN KEY (song_id) REFERENCES playlist (id) MATCH SIMPLE ON UPDATE NO ACTION ON DELETE CASCADE
+)
+WITH (
+	OIDS=FALSE
+);
+
