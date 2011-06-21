1bc8ed519dd45cc97ee255c275ac37201c62e966
Someone <someone@surgebot.net>
2011-06-21 14:52:00 +0200

    pushmode: Only push usermode if not already set

--- modules/pushmode/pushmode.c
+++ modules/pushmode/pushmode.c
@@ -5,6 +5,7 @@
 #include "ptrlist.h"
 #include "timer.h"
 #include "irc.h"
+#include "chanuser.h"
 #include "modules/pushmode/pushmode.h"
 
 MODULE_DEPENDS(NULL);
@@ -76,6 +77,38 @@ static void pushmode_free(struct delayed_mode *mode)
 
 void pushmode(struct irc_channel *channel, char *mode, const char *target)
 {
+	// is the mode and target setting a usermode
+	// -> if so, check if it has already been set
+	char *mode_bak = mode;
+	char sign = '+';
+	if(*mode_bak == '+' || *mode_bak == '-') {
+		sign = *mode_bak++;
+	}
+	// make sure there is exactly one mode specifier
+	assert(strlen(mode_bak) == 1);
+
+	if(*mode_bak == 'v' || *mode_bak == 'o') {
+		struct irc_user *ircuser = user_find(target);
+		if(!ircuser) {
+			debug("pushmode: User '%s' does not exist, tried to set usermode %s", target, mode);
+			return;
+		}
+		struct irc_chanuser *chanuser = channel_user_find(channel, ircuser);
+		if(!chanuser) {
+			debug("pushmode: User '%s' is not on %s, can't set usermode %s", target, channel->name, mode);
+			return;
+		}
+
+		int flag = 0;
+		if(*mode_bak == 'v') flag = MODE_VOICE;
+		else				 flag = MODE_OP;
+
+		if((sign == '+' && (chanuser->flags & flag)) || (sign == '-' && !(chanuser->flags & flag))) {
+			// mode already set, ignore
+			return;
+		}
+	}
+
 	// Is there a list of modes for this channel?
 	struct ptrlist *chanlist = dict_find(pushmode_dict, channel->name);
 	if(!chanlist) {
